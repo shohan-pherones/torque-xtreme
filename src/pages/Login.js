d7de4c638eb2d9a1c8650f1231c54d3f8a3742c2
shohan-pherones <shohan.sub.56@gmail.com>
2022-12-14 20:04:22 +0600

    login and register page added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,78 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = ({ navbarHeight }) => {
-  return <div style={{ marginTop: `${navbarHeight}px` }}>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div
+      className="hero min-h-screen bg-base-200"
+      style={{ marginTop: `${navbarHeight}px` }}
+    >
+      <div className="hero-content flex-col">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Login</h1>
+          <p className="py-6">
+            Please login to your account, and get access to our amazing
+            services.
+          </p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body" onSubmit={handleLogin}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                type="email"
+                placeholder="hello@example.com"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                type="password"
+                placeholder="write your password"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+              <p className="pt-2">
+                Don't have an account?{" "}
+                <Link
+                  to="/register"
+                  className="text-info underline underline-offset-2 hover:text-secondary"
+                >
+                  Register
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,92 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Register = ({ navbarHeight }) => {
-  return <div style={{ marginTop: `${navbarHeight}px` }}>Register</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(name, email, password);
+
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div
+      className="hero min-h-screen bg-base-200"
+      style={{ marginTop: `${navbarHeight}px` }}
+    >
+      <div className="hero-content flex-col">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Register</h1>
+          <p className="py-6">
+            Create your account, and get our amazing services today.
+          </p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body" onSubmit={handleRegister}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                type="text"
+                placeholder="Natasha Jennie"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                type="email"
+                placeholder="hello@example.com"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                type="password"
+                placeholder="write your password"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary">
+                Create account
+              </button>
+              <p className="pt-2">
+                Already have an account?{" "}
+                <Link
+                  to="/login"
+                  className="text-info underline underline-offset-2 hover:text-secondary"
+                >
+                  Login
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
