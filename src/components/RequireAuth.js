09e0a18e85e72fd930dbcc9632c74aa510573f36
shohan-pherones <shohan.sub.56@gmail.com>
2022-12-15 19:48:00 +0600

    verification email sent

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -1,12 +1,17 @@
-import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  useAuthState,
+  useSendEmailVerification,
+} from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
+import { toast } from "react-toastify";
 import auth from "../firebase.init";
 import Spinner from "./Spinner";
 
 const RequireAuth = ({ children }) => {
   const [user, loading] = useAuthState(auth);
-  console.log("inside req auth", user);
   const location = useLocation();
+  const [sendEmailVerification, sending, error] =
+    useSendEmailVerification(auth);
 
   if (loading) {
     return <Spinner />;
@@ -14,6 +19,27 @@ const RequireAuth = ({ children }) => {
 
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  if (!user.emailVerified) {
+    return (
+      <div className="mt-32 flex flex-col gap-5 items-center">
+        <h3 className="text-xl">
+          Your email is not verified, please verify your email address first.
+        </h3>
+        <button
+          className="btn"
+          onClick={async () => {
+            const success = await sendEmailVerification();
+            if (success) {
+              toast.info("Verification email sent!");
+            }
+          }}
+        >
+          Resend verification email
+        </button>
+      </div>
+    );
   }
 
   return children;
